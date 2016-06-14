ec4eb94b427e02f020d546b018abdf23ba5caece
Dylan Foster <dylan947@gmail.com>
2016-06-14 09:05:11 -0700

    feat(application): alias application#map to Router.map

--- test/application_tests.js
+++ test/application_tests.js
@@ -3,9 +3,20 @@
 import { expect } from "chai";
 
 import Application from "../src/application";
+import Router from "../src/router";
 
 describe("Application", function () {
+  let application;
+
   describe("#map", function () {
-    it("gets called with a Router instance");
+    beforeEach(function () {
+      application = new Application();
+    });
+
+    it("gets called with a Router instance", function () {
+      application.map(function () {
+        expect(this).to.be.an.instanceof(Router);
+      });
+    });
   });
 });
