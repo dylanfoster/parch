273b441750dbde140d9788c9948edcdf2413c443
Dylan Foster <dylan947@gmail.com>
2016-06-14 09:03:13 -0700

    test(router): ensure router instance is returned and passed to callback

--- test/router_tests.js
+++ test/router_tests.js
@@ -3,3 +3,19 @@
 import { expect } from "chai";
 
 import Router from "../src/router";
+
+describe("Router", function () {
+  describe("map", function () {
+    it("returns an instance of Router", function () {
+      const router = Router.map(function () {});
+
+      expect(router).to.be.an.instanceof(Router);
+    });
+
+    it("calls callback with an instance of Router", function () {
+      Router.map(function () {
+        expect(this).to.be.an.instanceof(Router);
+      });
+    });
+  });
+});
