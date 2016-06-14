3918f6de066621abc172a666d403a93c3fb2341d
Dylan Foster <dylan947@gmail.com>
2016-06-14 08:29:01 -0700

    feat(application): pending tests

---
+++ test/application_tests.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const chai = require("chai");
+const expect = chai.expect;
+
+const Application = require("../lib/application");
+
+describe("Application", function () {
+  describe("#map", function () {
+    it("gets called with a Router instance");
+  });
+});
