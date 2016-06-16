daf427cba827bf9be1b07b983ad2a1f5b383da2e
Dylan Foster <dylan947@gmail.com>
2016-06-15 17:43:26 -0700

    feat(application): pass through server options to restify

--- src/application.js
+++ src/application.js
@@ -5,16 +5,31 @@ import restify from "restify";
 import Loader from "./loader";
 import Router from "./router";
 
+/**
+ * TODO:
+ *   - add DEFAULT_CONTROLLER_LOOKUP_PATH
+ *     - should be <caller location>/contrllers
+ */
+
 class Application {
-  constructor(options) {
-    const app = options.app || restify.createServer();
+  constructor(options = {}) {
+    const serverOptions = options.server || {};
+
+    // TODO: merge with defaults
+    const middlewares = serverOptions.middlewares || [];
+    const app = options.app || restify.createServer(serverOptions);
+    const controllerLoader = new Loader({
+      type: "controller",
+      path: options.controllers.dir // || DEFAULT_CONTROLLER_LOOKUP_PATH
+    });
     const routerSettings = {
       app,
       loader: {
-        controllers: new Loader({ type: "controller", path: options.controllers.dir })
+        controllers: controllerLoader
       }
     };
 
+    middlewares.forEach(middlware => { app.use(middlware); });
     this.app = app;
     this.map = Router.map.bind(null, routerSettings);
   }
