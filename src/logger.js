d88b1d2be20d20918dce76e258708a543eaf967f
Dylan Foster <dylan947@gmail.com>
2016-06-30 09:42:34 -0700

    feat(logger): add logger class

---
+++ src/logger.js
@@ -0,0 +1,66 @@
+"use strict";
+
+import path from "path";
+
+import Bunyan from "bunyan";
+
+const LOG_ENV = process.env.NODE_ENV || "development";
+
+class Logger {
+  constructor(name, options = {}) {
+    name = name || "api";
+
+    const serializers = options.serializers || {};
+
+    if (!serializers.req) {
+      serializers.req = this.req;
+    }
+    if (!serializers.res) {
+      serializers.res = this.res;
+    }
+
+    const streams = [];
+
+    if (options.dir) {
+      streams.push({
+        path: path.join(options.dir, `${LOG_ENV}.log`)
+      });
+    }
+
+    if (process.env.NODE_ENV !== "test") {
+      streams.push({
+        level: "info",
+        stream: process.stdout
+      });
+    }
+
+    return Bunyan.createLogger({ name, serializers, streams });
+  }
+
+  req(req) {
+    if (!req) { return false; }
+
+    return {
+      httpVersion: req.httpVersion,
+      method: req.method,
+      params: req.params,
+      query: req.query,
+      url: req.url
+    };
+  }
+
+  res(res) {
+    if (!res) { return false; }
+
+    return {
+      statusCode: res.statusCode,
+      headers: res.headers
+    };
+  }
+
+  static create(name, options) {
+    return new Logger(name, options);
+  }
+}
+
+export default Logger;
