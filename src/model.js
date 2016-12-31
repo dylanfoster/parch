779dd53a7797a0d66ab41b280b568fe33eeac7dc
Dylan Foster <dylan947@gmail.com>
2016-12-31 15:56:06 -0800

    docs(model): document constructor and define

--- src/model.js
+++ src/model.js
@@ -2,18 +2,24 @@
 
 class Model {
   get name() {
-    /**
-     * TODO: we should probably just lowercase this and be done. This would mean
-     * models would be accessed in lowercase (e.g. models.user) and when creating
-     * the schema, tablenames would be lowercase always
-     */
     return this.constructor.name.split(/model/i)[0];
   }
 
+  /**
+   * constructor
+   *
+   * @param {Object} options sequelize model options
+   * @see http://docs.sequelizejs.com/en/v3/docs/models-definition/#configuration
+   */
   constructor(options = {}) {
     this.options = options;
   }
 
+  /**
+   * define
+   * @param {Object} DataTypes sequelize DataTypes Object
+   * @see http://docs.sequelizejs.com/en/v3/docs/models-definition/#data-types
+   */
   define() {
     throw new Error("Models must supply a 'define' method");
   }
