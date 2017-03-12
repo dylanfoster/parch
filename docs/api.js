98181bec032a056a18356f56620525c508abca60
Dylan Foster <dylan947@gmail.com>
2017-03-12 14:33:24 -0700

    chore: unignore docs

---
+++ docs/api.js
@@ -0,0 +1,24 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Application",
+        "Controller",
+        "Model",
+        "ModelManager",
+        "Route",
+        "RouteSegment",
+        "Router"
+    ],
+    "modules": [
+        "parch"
+    ],
+    "allModules": [
+        {
+            "displayName": "parch",
+            "name": "parch",
+            "description": "Base application"
+        }
+    ],
+    "elements": []
+} };
+});
