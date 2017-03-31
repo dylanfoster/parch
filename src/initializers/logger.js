1711dcc6d64b36a700b2097c9fad5f5a1b0ad342
Dylan Foster <dylan947@gmail.com>
2017-03-30 18:35:23 -0700

    chore: deprecate config.log

--- src/initializers/logger.js
+++ src/initializers/logger.js
@@ -1,11 +1,14 @@
 "use strict";
 
 import Bunyan from "bunyan";
+import depd from "depd";
 
 module.exports = {
   initialize(application, registry) {
     const Logger = registry.lookup("module:logger");
     const config = registry.lookup("config:main");
+
+    depd("parch")("config#log is deprectated and will be removed in 2.0.0");
 
     if (config.log && config.log instanceof Bunyan) {
       registry.register("service:logger", config.log);
