9d9b22d37dc5537c2a6c17535d9cc2ee9481178c
Dylan Foster <dylan947@gmail.com>
2017-04-02 12:42:38 -0700

    feat(store): add store to main export

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Controller = require("./lib/controller");
 const Model = require("./lib/model");
 const Route = require("./lib/route");
 const RouteSegment = require("./lib/route_segment");
+const Store = require("./lib/store");
 const containment = require("./lib/containment");
 
 /**
@@ -16,5 +17,6 @@ module.exports = {
   Model,
   Route,
   RouteSegment,
+  Store,
   containment
 };
