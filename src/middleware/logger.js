f8dd114cfa5b9a9a568d120b6497dbb52042f5fe
Dylan Foster <dylan947@gmail.com>
2017-11-11 19:52:11 -0800

    feat(middleware): remove info log from log middleware
    fixes #56

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -4,7 +4,6 @@ export default function (options) {
   return function logger(req, res, next) {
     const log = options.log;
 
-    log.child({ reqID: req.getId() }).info({ req, res });
     req.log = log.child({ reqID: req.getId() });
     next();
   };
